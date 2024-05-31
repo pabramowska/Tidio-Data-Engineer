9ec1bb7a055a033e4d982755006e18b42e6228bb
pabramowska <93685039+pabramowska@users.noreply.github.com>
2024-05-31 14:53:28 +0200

    updated avg_message_volume

--- task2/task2.sql
+++ task2/task2.sql
@@ -1,5 +1,4 @@
-select  ppv.customer_id, pp.project_id, customers_email.customer_email,
-MAX(CASE WHEN pp.label = 'avg_message_volume' THEN CAST(ppv.value AS DECIMAL) END) AS avg_message_volume,
+select  ppv.customer_id, pp.project_id, email.customer_email, volume.avg_message_volume,
 MAX(CASE WHEN pp.label = 'estimated_client_volume_usd' THEN CAST(ppv.value AS DECIMAL) END) AS estimated_client_volume_usd,
 MAX(CASE WHEN pp.label = 'plan' THEN ppv.value END) AS plan,
 MAX(CASE WHEN pp.label = 'interested_in_product' THEN ppv.value END) AS interested_in_product
@@ -13,24 +12,19 @@ JOIN(
 FROM rekrutacja.project_properties_values as ppv
 JOIN
     rekrutacja.project_properties pp ON ppv.property_id = pp.id 
-WHERE pp.label = 'e-mail' and ppv.customer_id) customers_email ON ppv.customer_id = customers_email.customer_id AND customers_email.row_num = 1
-JOIN (
-    SELECT
-        ppv.customer_id,
-        ppv.property_id,
-        MAX(create_dte) AS latest_create_dte
-    FROM
-        rekrutacja.project_properties_values as ppv
-    GROUP BY
-        customer_id, property_id
-) latest_values ON ppv.customer_id = latest_values.customer_id
-                 AND ppv.property_id = latest_values.property_id
-                 AND ppv.create_dte = latest_values.latest_create_dte
-                 
+WHERE pp.label = 'e-mail' and ppv.customer_id) email ON ppv.customer_id = email.customer_id AND email.row_num = 1        
+JOIN(
+	SELECT ppv.customer_id, (CASE WHEN pp.label = 'avg_message_volume' THEN CAST(ppv.value AS DECIMAL) END) AS avg_message_volume, 
+    row_number() over (PARTITION BY ppv.customer_id order by ppv.create_dte desc) as row_num
+FROM rekrutacja.project_properties_values as ppv
+JOIN
+    rekrutacja.project_properties pp ON ppv.property_id = pp.id 
+WHERE pp.label = 'avg_message_volume' and ppv.customer_id) volume ON ppv.customer_id = volume.customer_id AND volume.row_num = 1            
 GROUP BY
     pp.project_id,
     ppv.customer_id,
-    customers_email.customer_email
+    email.customer_email,
+    volume.avg_message_volume
 HAVING
     avg_message_volume > 5000 AND
     estimated_client_volume_usd > 1000
