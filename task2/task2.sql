11d934f48f20d8d417c0167ca798139b1bcdff53
pabramowska <93685039+pabramowska@users.noreply.github.com>
2024-05-31 14:00:27 +0200

    latest email

--- task2/task2.sql
+++ task2/task2.sql
@@ -1,9 +1,22 @@
-select  ppv.customer_id,
+select  ppv.customer_id, pp.project_id,
 MAX(CASE WHEN pp.label = 'e-mail' THEN ppv.value END) AS customer_email
 
 FROM
     rekrutacja.project_properties_values ppv
 JOIN
     rekrutacja.project_properties pp ON ppv.property_id = pp.id 
+JOIN (
+    SELECT
+        ppv.customer_id,
+        ppv.property_id,
+        MAX(create_dte) AS latest_create_dte
+    FROM
+        rekrutacja.project_properties_values as ppv
+    GROUP BY
+        customer_id, property_id
+) latest_values ON ppv.customer_id = latest_values.customer_id
+                 AND ppv.property_id = latest_values.property_id
+                 AND ppv.create_dte = latest_values.latest_create_dte
 GROUP BY
-	customer_id
+    pp.project_id,
+    ppv.customer_id
